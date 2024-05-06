9545fd4d90a2cccf97972cf9fb5a20ef9c00bacd
Sahil-Dash <sahildash386@gmail.com>
2024-05-06 22:36:01 +0530

    implement initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signin = () => {
+  return (
+    <div>
+      this is sign in
+    </div>
+  )
+}
+
+export default signin
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>
+        this is signup
+      
+    </div>
+  )
+}
+
+export default signup
